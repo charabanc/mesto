03beb09837b07f25ec004f2d7281df691ec4a8d2
Air-Magomed <59258850+charabanc@users.noreply.github.com>
2021-02-23 22:12:18 +0300

    Main

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const buttonEdit = document.querySelector('.button__edit');
+const popup = document.querySelector('.popup');
+
+const closeIcon = document.querySelector('.popup__closeIcon');
+
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.nameInput');
+const jobInput = document.querySelector('.jobInput');
+
+const profileName = document.querySelector('.profile__name_text');
+const profileStatus = document.querySelector('.profile__status');
+
+buttonEdit.addEventListener( 'click', function(){
+  popup.classList.add('shown');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileStatus.textContent;
+});
+
+closeIcon.addEventListener('click', function(){
+  popup.classList.remove('shown');
+});
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileStatus.textContent = jobInput.value;
+  popup.classList.remove('shown');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
