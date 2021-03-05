29c77bc65e487c98941f953489fb8e608856c314
Air-Magomed <59258850+charabanc@users.noreply.github.com>
2021-03-05 11:50:50 +0400

    Код ревью 3

--- script/index.js
+++ script/index.js
@@ -3,32 +3,30 @@ const popup = document.querySelector('.popup');
 
 const closeIcon = document.querySelector('.popup__close-icon');
 
-const formElement = document.querySelector('.popup__container');
-const nameInput = document.querySelector('.popup__input-name');
-const jobInput = document.querySelector('.popup__input-job');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_job');
 
 const profileName = document.querySelector('.profile__info-name');
 const profileStatus = document.querySelector('.profile__info-data');
+const formElement = document.querySelector('.popup__container');
 
 function popupOpenedAdd (){
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileStatus.textContent;
 };
 
-buttonEdit.addEventListener( 'click', popupOpenedAdd);
-
 function popupOpenedRemove (){
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 };
-
-closeIcon.addEventListener('click', popupOpenedRemove);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 };
 
+buttonEdit.addEventListener( 'click', popupOpenedAdd);
+closeIcon.addEventListener('click', popupOpenedRemove);
 formElement.addEventListener('submit', formSubmitHandler);
